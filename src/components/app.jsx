20a7c29fefc4a893a0d0c3ddd5678ca47bea8965
naveed <muhammad.naveed@cloudcall.com>
2018-02-02 12:58:37 -0800

    adding filter dropdown

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import SearchBox from './searchbox';
 import DataTable from './datatable';
 import Pagination from './pagination';
+import DropDownMenu from './dropdownmenu';
 
 
 import { RingLoader } from 'react-spinners';
@@ -15,6 +16,7 @@ class DataGrid extends React.Component {
 
     this.state = {
       data: props.data,
+      types:props.types,
       page: props.page,
       displayCount: props.displayCount,
       searchTerm: props.searchTerm,
@@ -24,6 +26,11 @@ class DataGrid extends React.Component {
 
   }
 
+  updateSettings(type, value) {
+    var setting = {};
+    setting[type] = value
+   // this.props.onChange(setting);
+  }
 
 
   // fetch(proxyUrl + targetUrl)
@@ -39,13 +46,19 @@ class DataGrid extends React.Component {
   componentDidMount() {
     const proxyUrl = 'https://cryptic-spire-94950.herokuapp.com/';
     const targetUrl = 'https://agriplacex-organizations.herokuapp.com/api/v1/organizations/';
-    fetch(proxyUrl + targetUrl)
-      .then(res => res.json())
-      .then(
+    const orgUrl =  'https://agriplacex-organizations.herokuapp.com/api/v1/organization-types/';
+
+
+    let p4 = Promise.all([fetch(proxyUrl + targetUrl).then(res => res.json()),fetch(proxyUrl+orgUrl).then(res => res.json())]);
+    //fetch(proxyUrl + targetUrl)
+      //.then(res => res.json())
+      p4.then(
       (res) => {
         console.log(res);
+        let orgType=res[1].results.map((k)=>k.name);
         this.setState({
-          data: res.results,
+          data: res[0].results,
+          types:orgType,
           isLoading: false
         });
       },
@@ -91,10 +104,18 @@ class DataGrid extends React.Component {
             <SearchBox onChange={this.setState.bind(this)} searchTerm={this.state.searchTerm} />
           </div>
         </div>
+        <div className="row">
+          <div className="col-md-9">
+            
+          </div>
+          <div className="col-md-3 ">
+          <DropDownMenu value={this.state.types[0]} options={this.state.types} ref="page" onChange={this.updateSettings.bind(this)} />
+          </div>
+        </div>
         <div className="dataTable">
           <DataTable
             rows={paginated.paginatedData}
-            cols={[{"name":"Name"},{"city":"City"},{"business_identification_number":"Buiness #"},
+            cols={[{"name":"Name"},{"city":"City"},{"country":"Country"},{"business_identification_number":"Buiness #"},
             {"ggn_number":"GGN #"},{"phone":"Phone"},{"email":"Email"},{"detail":"Detail"}]}
              onChange={this.setState.bind(this)}
             // sort={this.state.sort}
@@ -139,6 +160,7 @@ class DataGrid extends React.Component {
 
 DataGrid.defaultProps = {
   data: [],
+  types:[],
   displayCount: 10,
   page: 1,
   searchTerm: "",
